f9d61b720cd9322b3c87de99b17ea7a4ed3ce46f
Mike-Pits <mgp0227@gmail.com>
2024-06-02 14:12:47 +0300

    update 020624

--- scripts/clip_func.py
+++ scripts/clip_func.py
@@ -13,11 +13,12 @@ def clip():
             df.iloc[0, int(list_data[0][i].split()[0])] = ' '.join(list_data[0][i].split()[1:][:])
         elif list_data[0][i].split()[0] == 'т/к':
             df.iloc[0, 0] = list_data[0][i][4:]
-        elif list_data[0][i].split()[0] == 'Дисп.':
-            df.iloc[0, 102] = list_data[0][i][6:]
+        elif list_data[0][i].split()[0].lower() == 'дисп':
+            df.iloc[0, 102] = list_data[0][i].split()[1]
         else:
             df.iloc[0, 103] = list_data[0][i]
     
     return df
 
 
+print(clip())
